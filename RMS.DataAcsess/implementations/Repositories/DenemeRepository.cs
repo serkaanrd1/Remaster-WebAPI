0c6e48772a25c89036901154eb38eb7e2d7c9389
serkaanrd1 <143171559+serkaanrd1@users.noreply.github.com>
2023-10-19 14:54:09 +0300

    Remaster WebAPI
    Araç yazılım şirketi için geliştirdiğim WebAPI projesi.

---
+++ RMS.DataAcsess/implementations/Repositories/DenemeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.DataAccess.EF;
+using RMS.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.DataAcsess.implementations.Repositories
+{
+    public class DenemeRepository : BaseRepository<Deneme>
+    {
+    }
+}
